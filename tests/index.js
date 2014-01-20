72ad6f4ec3624d6ca484b1e6883cfe74b62c1e7c
Yoshua Wuyts <yoshuawuyts@gmail.com>
2014-01-20 12:43:56 +0100

    add tests package

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,15 @@
 /**
  * Module dependencies
  */
-var models = require('../client/modules/models');
+ 
+var fixtures = require('./fixtures/fixtures');
+var Backbone = require('backbone');
 var should = require('should');
 var sinon = require('sinon');
+var app = require('../build/scripts/scripts.js')
+
+describe("Application", function() {
+  it("creates a global variable for the name space", function () {
+    app.should.exist;
+  })
+})
