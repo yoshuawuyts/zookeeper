c2e9fefe8e36f7c6b8a8dd26d26a1defff7b7b51
Yoshua Wuyts <yoshuawuyts@gmail.com>
2014-01-29 11:24:51 +0100

    Update gulpfile: packaged dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,11 @@
  */
 
 var gulp = require('gulp');
+var path = require('centurion');
+var gulpUtil = require('gulp-util');
 
-/**
- * Import tasks
- */
-
-require('./tasks/lint');    // esformatter, eslint
-require('./tasks/modules'); // browserify, uglify
-require('./tasks/server');  // nodemon
-require('./tasks/static');  // copy fonts + images
-require('./tasks/styles');  // rework, autoprefixer, csso
-require('./tasks/tests');   // mocha
-require('./tasks/vendor');  // concat
-
-/**
- * Default task
- */
-
-gulp.task('default', function() {
-  gulp.run( 
-    'vendor',
-    'modules',
-    'static',
-    'styles',
-    'lint'
-    // 'tests' 
-  );
-  gulp.run(
-    'server'
-  );
-});
+try {
+  require(path.resolve(__dirname, gulpUtil.env.gulpfile));
+} catch (err) {
+  console.error('Unable to load %s', gulpUtil.env.gulpfile);
+}
