cdcdbf01a3f8a19622e2a9c0de372ab01961aeab
yoshuawuyts <i@yoshuawuyts.com>
2014-02-26 13:29:02 +0100

    update client packages

--- gulpfile.js
+++ gulpfile.js
@@ -26,10 +26,9 @@ gulp.task('static', function() {
   gulp.src("client/vendor/localforage/dist/backbone.localforage.js").pipe(gulp.dest("build/vendor"));
   gulp.src("client/vendor/lodash/dist/lodash.underscore.min.js").pipe(gulp.dest("build/vendor"));
   gulp.src("client/vendor/localforage/dist/localforage.js").pipe(gulp.dest("build/vendor"));
-  gulp.src("client/vendor/backbone/backbone-min.map").pipe(gulp.dest("build/vendor"));
-  gulp.src("client/vendor/backbone/backbone-min.js").pipe(gulp.dest("build/vendor"));
-  gulp.src("client/vendor/jquery/jquery.min.map").pipe(gulp.dest("build/vendor"));
-  gulp.src("client/vendor/jquery/jquery.min.js").pipe(gulp.dest("build/vendor"));
+  gulp.src("client/vendor/backbone/backbone.js").pipe(gulp.dest("build/vendor"));
+  gulp.src("client/vendor/jquery/dist/jquery.min.map").pipe(gulp.dest("build/vendor"));
+  gulp.src("client/vendor/jquery/dist/jquery.min.js").pipe(gulp.dest("build/vendor"));
   gulp.src("client/vendor/react/react.min.js").pipe(gulp.dest("build/vendor"));
   gulp.src('client/images/**').pipe(gulp.dest('build/images/'));
   gulp.src('client/fonts/**').pipe(gulp.dest('build/fonts'));
